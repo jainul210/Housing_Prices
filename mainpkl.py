7bc94499b2a7e6b78ee7154d87b08864fd0a31f3
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2026-07-26 01:05:14 +0530

    feat: add housing dataset, pipeline, script, and update gitignore

---
+++ mainpkl.py
@@ -0,0 +1,78 @@
+ # pyrefly: ignore [missing-import]
+from sklearn.metrics import root_mean_squared_error
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+import os
+import joblib
+
+MODEL_FILE = "model.pkl"
+PIPELINE_FILE = "pipeline.pkl"
+
+def build_pipeline(num_attributes, cat_attributes):
+    num_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scalar", StandardScaler())
+    ])
+
+    cat_pipeline = Pipeline([
+        ("encoder", OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    full_pipeline = ColumnTransformer([
+        ('num', num_pipeline, num_attributes),
+        ('cat', cat_pipeline, cat_attributes)
+    ])
+    return full_pipeline
+
+if not os.path.exists(MODEL_FILE):
+    housing = pd.read_csv('housing.csv')
+
+    housing['income_cat'] = pd.cut(housing["median_income"], 
+    bins = [0, 1.5, 3, 4.5, 6, np.inf], 
+    labels= [1,2,3,4,5])
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+    for train_index, test_index in split.split(housing, housing['income_cat']):
+        housing.loc[test_index].drop('income_cat', axis= 1).to_csv("input.csv", index = False)
+        housing = housing.loc[train_index].drop('income_cat', axis= 1)
+
+    housing_lables = housing['median_house_value'].copy()
+    housing_features = housing.drop('median_house_value', axis=1)
+
+    num_attributes = housing_features.drop('ocean_proximity', axis=1).columns.tolist()
+    cat_attributes = ["ocean_proximity"]
+
+    pipeline = build_pipeline(num_attributes, cat_attributes)
+
+    housin_prepared = pipeline.fit_transform(housing)
+    # print(housin_prepared)
+
+    model = RandomForestRegressor(random_state=42)
+    model.fit(housin_prepared, housing_lables)
+
+    joblib.dump(model, MODEL_FILE)
+    joblib.dump(pipeline, PIPELINE_FILE)
+    print("Model is Trained")
+
+else:
+    model = joblib.load(MODEL_FILE)
+    pipeline = joblib.load(PIPELINE_FILE)
+
+    input_data = pd.read_csv("input.csv")
+    transformed_input = pipeline.transform(input_data)
+    predictions = model.predict(transformed_input)
+    input_data["median_house_value"] = predictions
+
+    input_data.to_csv('output.csv', index=False)
+    print("Inference is Done")
